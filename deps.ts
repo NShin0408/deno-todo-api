a73753dc60990f2cd2c1a4ed2421446a8ffb9da1
NTetora <38845423+NTetora@users.noreply.github.com>
2021-11-21 23:00:12 +0900

    add todo

--- deps.ts
+++ deps.ts
@@ -4,12 +4,15 @@ export {
     Status,
     Context,
     isHttpError
-} from "https://deno.land/x/oak@v10.0.0/mod.ts";
+} from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export {
     bold,
     yellow,
     green,
     cyan
 } from "https://deno.land/std@0.114.0/fmt/colors.ts";
+
+export { v4 as uuid} from "https://deno.land/std@0.115.0/uuid/mod.ts";
+
 
 export type { RouterContext } from "https://deno.land/x/oak@v10.0.0/mod.ts";
--- scripts.config.ts
+++ scripts.config.ts
@@ -5,7 +5,7 @@ const config: DenonConfig = {
     start: {
       cmd: "deno run server.ts",
       desc: "run my server.ts file",
-      allow: ["net","env","read"],
+      allow: ["net","env","read","write"],
     },
   },
   watcher: {
